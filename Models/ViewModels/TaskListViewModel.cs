3d1d6d3f6cb5d1264102bed47b4575c624b0ab5f
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2026-04-21 21:45:32 +0600

    add view list for filter and Dashboard counter

---
+++ Models/ViewModels/TaskListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskTracker.Models.ViewModels;
+
+public class TaskListViewModel
+{
+    public IEnumerable<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    public string? SearchTerm { get; set; }
+    public string? FilterStatus { get; set; }
+    public string? SortOrder { get; set; }
+
+    public int TotalCount { get; set; }
+    public int CompletedCount { get; set; }
+    public int PendingCount { get; set; }
+}
