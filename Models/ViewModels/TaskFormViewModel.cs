837cf49d06c4cdd3dd5e7b052b96508b36e0cc42
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2026-04-21 21:39:10 +0600

    migration is done now need to set up the logic build up

---
+++ Models/ViewModels/TaskFormViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Models.ViewModels;
+
+public class TaskFormViewModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+    [Display(Name = "Title")]
+    public string Title { get; set; } = string.Empty;
+
+    [Display(Name = "Description")]
+    public string? Description { get; set; }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Due Date")]
+    public DateTime? DueDate { get; set; }
+
+    [Required(ErrorMessage = "Priority is required")]
+    [Display(Name = "Priority")]
+    public Priority Priority { get; set; } = Priority.Low;
+
+    [Display(Name = "Is Completed")]
+    public bool IsCompleted { get; set; } = false;
+}
